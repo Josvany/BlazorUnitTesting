a22eb505e27a756bdba59f2b0fadc9c992db3e49
Josvany Antonio García Peña <josvanyg@gmail.com>
2025-02-21 13:30:58 -0600

    parameters testing

--- BlazorStudentApp.Tests/Module3Test.cs
+++ BlazorStudentApp.Tests/Module3Test.cs
@@ -1,4 +1,9 @@
+using BlazorStudentApp.Data.Models;
+using BlazorStudentApp.Data.Services;
+using BlazorStudentApp.Pages.Student;
 using Bunit;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
 
 namespace BlazorStudentApp.Tests
 {
@@ -12,7 +17,6 @@ namespace BlazorStudentApp.Tests
             var renderedComponent = ctx.RenderComponent<Pages.Index>();
 
             renderedComponent.MarkupMatches("<h1>Welcome to our school</h1>\r\n<p>This is the official page to manage students of our School.</p>");
-
         }
 
         [Fact]
@@ -26,13 +30,68 @@ namespace BlazorStudentApp.Tests
 
             h1Element.MarkupMatches("<h1>Welcome to our school</h1");
 
-
-
             var pElement = renderedComponent.Find("p");
 
             pElement.MarkupMatches("<p>This is the official page to manage students of our School.</p>");
+        }
+        [Fact]
+        public void Parameter_Passing_DetailsComponentCorrectly_Test()
+        {
+            using var ctx = new TestContext();
+            var expectedHeading = "Student Details";
+            var expectedStudent = new Student
+            {
+                Id = 1,
+                Name = "Ervis Trupja",
+                Email = "ervis@trupja.com",
+                Phone = "12345678",
+                Address = "Address sample value"
+            };
 
+            var rC = ctx.RenderComponent<DetailsStudent>(p => p
+           .Add(n => n.Heading, expectedHeading)
+           .Add(n => n.SelectedStudent, expectedStudent)
+           );
+
+
+            //Assert
+            Assert.Contains(@"<h4 class=""modal-title"">" + expectedHeading + "</h4>", rC.Markup);
+            Assert.Contains(@"<p class=""card-text"">" + expectedStudent.Address + "</p>", rC.Markup);
+            Assert.Contains(@"<h5 class=""card-title"">" + expectedStudent.Name + "</h5>", rC.Markup);
+
+            Assert.Equal(expectedHeading, rC.Instance.Heading);
+            Assert.Equal(expectedStudent, rC.Instance.SelectedStudent);
 
         }
+
+        [Fact]
+        public void Service_Injection_AllStudentsComponentRendersCorrently_Test()
+        {
+            //Arrange
+            var studentsList = new List<Student>()
+            {
+                new()
+                {
+                    Id = 1,
+                    Name = "Ervis Trupja",
+                    Email = "ervis@trupja.com",
+                    Phone = "12345678",
+                    Address = "Address sample value"
+                }
+            };
+            var mockedService = new Mock<IStudentsService>();
+            mockedService.Setup(s => s.GetStudentsAsync()).ReturnsAsync(studentsList);
+
+            using var ctx = new TestContext();
+            ctx.Services.AddScoped(provider => mockedService.Object);
+
+            //Act
+            var renderedComponent = ctx.RenderComponent<AllStudents>();
+
+            //Assert
+            Assert.Equal(studentsList.Count, renderedComponent.Instance.students?.Count);
+        }
+
+
     }
 }
---
+++ BlazorStudentApp/Pages/Student/AllStudents.razor.cs
@@ -0,0 +1,48 @@
+﻿using BlazorStudentApp.Data.Services;
+using Microsoft.JSInterop;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorStudentApp.Pages.Student
+{
+    public partial class AllStudents
+    {
+
+        [Inject]
+        public required IStudentsService StudentsService { get; set; }
+        [Inject]
+        public IJSRuntime? JsRuntime { get; set; }
+        [Inject]
+        public NavigationManager? _navManager { get; set; }
+
+        public List<Data.Models.Student>? students;
+        private Data.Models.Student? selectedStudent;
+        bool showModal = false;
+
+        protected override async Task OnInitializedAsync()
+        {
+            students = await StudentsService.GetStudentsAsync();
+        }
+
+        protected void DeleteStudentConfirmation(Data.Models.Student student)
+        {
+            selectedStudent = student;
+            showModal = true;
+        }
+
+        protected void EditStudentConfirmationRedirect(Data.Models.Student student)
+        {
+            _navManager?.NavigateTo("/edit/" + student.Id, true);
+        }
+
+
+
+        void CancelDelete() => showModal = false;
+        async Task ConfirmDelete()
+        {
+            await StudentsService.DeleteStudentAsync(selectedStudent.Id);
+            students = await StudentsService.GetStudentsAsync();
+            showModal = false;
+        }
+
+    }
+}
